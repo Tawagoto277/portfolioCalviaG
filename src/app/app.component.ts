8bf6755f61bfb6111c5d9619b3a8b405331e85ca
TawaGC <giucalvia99@gmail.com>
2024-10-02 10:10:43 +0200

    portflio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, AfterViewChecked, OnInit{
   title = 'portfolioCalviaG';
+
+  currentSectionIndex: number = 0;
+  sections: HTMLElement[] = [];
+  contentDiv!: HTMLElement;
+
+  ngOnInit(): void {
+    // console.log("OnInit");
+    this.contentDiv = document.getElementById('content')!;
+    this.sections = Array.from(document.querySelectorAll('section'));
+  }
+
+  ngAfterViewInit(): void {
+    // console.log("Viewinit");
+    this.showSection(this.currentSectionIndex);
+  }
+
+  ngAfterViewChecked(){
+    // console.log("ViewCheck");
+    this.contentDiv.addEventListener('scroll', () => this.onDivScroll());
+  }
+
+  showSection(index: number){
+    this.sections.forEach((section, idx) => {
+      section.classList.toggle('active', idx === index);
+    });
+  }
+
+  onDivScroll(){
+    const scrollPosition = this.contentDiv.scrollTop;
+    // console.log("position" + scrollPosition);    
+    const windowHeight = this.contentDiv.clientHeight;
+    // console.log("altezza" + windowHeight);    
+    const newIndex = Math.floor(scrollPosition / windowHeight);
+    // console.log("index" + newIndex);
+    
+    if (newIndex !== this.currentSectionIndex) {
+      this.currentSectionIndex = newIndex;
+      this.showSection(this.currentSectionIndex);
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
